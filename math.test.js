f417572aad6acf5e53821a7c3092a9b22d5ab9da
Utsho11 <utshoroy521@gmail.com>
2025-10-25 23:14:38 +0600

    run test using AAA pattern

--- math.test.js
+++ math.test.js
@@ -1,7 +1,38 @@
-import { expect, test } from "vitest";
+import { expect, it, test } from "vitest";
 import { add } from "./math";
 
-test("adds 1 + 2 to equal 3", () => {
-  const array = [1, 2];
-  expect(add(array)).toBe(3);
+// test("adds 1 + 2 to equal 3", () => {
+//   const array = [1, 2];
+//   expect(add(array)).toBe(3);
+// });
+
+// AAA pattern
+it("should return the sum of an array of numbers", () => {
+  // Arrange
+  const arr = [1, 3, 2];
+  const expectedResult = arr.reduce((acc, curr) => acc + curr, 0);
+  // Action
+  const result = add(arr);
+  // Assertion
+  expect(result).toBe(expectedResult);
+});
+
+it("should return NaN if we provide an array with non-numeric values", () => {
+  // Arrange
+  const arr = [1, " invalid", 2];
+  //   const expectedResult = arr.reduce((acc, curr) => acc + curr, 0);
+  // Action
+  const result = add(arr);
+  // Assertion
+  expect(result).toBeNaN();
+});
+
+it("should return sum if we provide an array with numeric string", () => {
+  // Arrange
+  const arr = ["1", " 3", "2"];
+  const expectedResult = arr.reduce((acc, curr) => acc + Number(curr), 0);
+  // Action
+  const result = add(arr);
+  // Assertion
+  expect(result).toBe(expectedResult);
 });
